0e1245629b5ed659b4e6a7c9e60154a7dc3c228c
Bulzan Stefan <stefan.bulzan@nordlogic.com>
2021-07-26 19:20:16 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curs16-checkpoints';
+  sidebarOpened = false;
+
+  toggleSidebar(event: any) {
+    console.log("hello")
+    this.sidebarOpened = !this.sidebarOpened;
+  }
 }
